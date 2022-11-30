b842f334de7848404f78782ba65dec5e510c4709
SangHun <shzero211@naver.com>
2022-11-30 23:58:42 +0900

    feat: tony->출석체크

--- src/main/java/tony/prefix_sum/출석체크_20438.java
+++ src/main/java/tony/prefix_sum/출석체크_20438.java
@@ -13,6 +13,7 @@ public class 출석체크_20438 {
         int sleepStuNum=Integer.parseInt(st.nextToken());
         int checkStuNum=Integer.parseInt(st.nextToken());
         int line=Integer.parseInt(st.nextToken());
+        int add,s,e=0;
         int[] dp=new int[stuNum+3];
         boolean[] sleep,chk;
         for(int i=3;i<=stuNum+2;i++){
@@ -28,6 +29,31 @@ public class 출석체크_20438 {
         st=new StringTokenizer(br.readLine());
         for(int i=0;i<checkStuNum;i++){
             int x=Integer.parseInt(st.nextToken());
+            if(sleep[x])continue;
+            add=x;
+            while(x<=stuNum+2){
+                if(sleep[x]){
+                    x+=add;
+                }else{
+                    chk[x]=true;
+                    x+=add;
+                }
+            }
+        }
+        for(int i=3;i<=checkStuNum+2;i++){
+            int a;
+            if(!chk[i]){
+                a=1;
+            }else{
+                a=0;
+            }
+            dp[i]=dp[i-1]+a;
+        }
+        for(int i=0;i<line;i++){
+            st=new StringTokenizer(br.readLine());
+            s=Integer.parseInt(st.nextToken());
+            e=Integer.parseInt(st.nextToken());
+            System.out.println(dp[e]-dp[s-1]);
         }
 
     }
