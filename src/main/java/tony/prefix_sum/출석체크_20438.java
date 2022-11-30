d7baf99334a80b7ad3e33983c87cfb4c2dc23a05
SangHun <shzero211@naver.com>
2022-12-01 00:30:24 +0900

    refactor : tony->출석체크 수정

--- src/main/java/tony/prefix_sum/출석체크_20438.java
+++ src/main/java/tony/prefix_sum/출석체크_20438.java
@@ -3,58 +3,51 @@ package tony.prefix_sum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 출석체크_20438 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int stuNum=Integer.parseInt(st.nextToken());
-        int sleepStuNum=Integer.parseInt(st.nextToken());
-        int checkStuNum=Integer.parseInt(st.nextToken());
-        int line=Integer.parseInt(st.nextToken());
-        int add,s,e=0;
-        int[] dp=new int[stuNum+3];
-        boolean[] sleep,chk;
-        for(int i=3;i<=stuNum+2;i++){
-            dp[i]=i;
-        }
-        sleep=new boolean[5005];
-        chk=new boolean[5005];
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer(br.readLine());
+    int stuN=Integer.parseInt(st.nextToken());//학생수
+    int sleepN=Integer.parseInt(st.nextToken());//졸고있는 학생수
+    int visitN=Integer.parseInt(st.nextToken());//출석코드 보낸수
+    int lineN=Integer.parseInt(st.nextToken());//주어진 구간 수
+        boolean[] v=new boolean[stuN+3];//출석 체크 표시
+    List<Integer> sleepList=new ArrayList<>();
+    st=new StringTokenizer(br.readLine());
+    for(int i=0;i<sleepN;i++){
+        sleepList.add(Integer.parseInt(st.nextToken()));
+    }
         st=new StringTokenizer(br.readLine());
-        for(int i=0;i<sleepStuNum;i++){
+        for(int i=0;i<visitN;i++){
             int x=Integer.parseInt(st.nextToken());
-            sleep[x]=true;
-        }
-        st=new StringTokenizer(br.readLine());
-        for(int i=0;i<checkStuNum;i++){
-            int x=Integer.parseInt(st.nextToken());
-            if(sleep[x])continue;
-            add=x;
-            while(x<=stuNum+2){
-                if(sleep[x]){
-                    x+=add;
-                }else{
-                    chk[x]=true;
-                    x+=add;
+            if(sleepList.contains(x)) continue;
+            for(int j=x;j<stuN+3;j++){
+                //배수이면
+                if(j%x==0){
+                    if(!sleepList.contains(j)){
+                        v[j]=true;
+                    }
                 }
             }
         }
-        for(int i=3;i<=checkStuNum+2;i++){
-            int a;
-            if(!chk[i]){
-                a=1;
-            }else{
-                a=0;
-            }
-            dp[i]=dp[i-1]+a;
+        int[] cumSum=new int[stuN+3];
+        for(int i=3;i<cumSum.length;i++){
+            cumSum[i]=cumSum[i-1]+(v[i]?0:1);
         }
-        for(int i=0;i<line;i++){
+        for(int i=0;i<lineN;i++){
             st=new StringTokenizer(br.readLine());
-            s=Integer.parseInt(st.nextToken());
-            e=Integer.parseInt(st.nextToken());
-            System.out.println(dp[e]-dp[s-1]);
+            int s=Integer.parseInt(st.nextToken());
+            int e=Integer.parseInt(st.nextToken());
+            System.out.println(cumSum[e]-cumSum[s-1]);
         }
+
+
+
+
 
     }
 }
